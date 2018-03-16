12e33abd24b505954d2bad0d4907e2274d5c0d32
JanikRitz <janikritz@gmail.com>
2018-03-16 10:09:13 +0100

    Comments Clarification

--- src/DifferentialPilotFactory.java
+++ src/DifferentialPilotFactory.java
@@ -7,9 +7,11 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class DifferentialPilotFactory {
 
     public static DifferentialPilot newMasterPilot(){
+        // 35mm Diameter + 3/5 Gear Ratio + 10% Correction
         return new DifferentialPilot((35.0*(3.0/5.0)/1.1), 136.0, Motor.B, Motor.C);
     }
     public static DifferentialPilot newSlavePilot(){
+        // 56mm Diameter + 3% Correction
         return new DifferentialPilot(56.0/1.03, 115.0, Motor.B, Motor.C);
     }
 }
