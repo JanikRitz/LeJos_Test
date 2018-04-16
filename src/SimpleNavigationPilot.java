c1d2f7cabfc74ea1568225806b541236abf29528
IsabellCM <isabell@muszynski.de>
2018-04-16 11:17:43 +0200

    Simple Navigation for Test Purposes only

---
+++ src/SimpleNavigationPilot.java
@@ -0,0 +1,38 @@
+import lejos.robotics.navigation.DifferentialPilot;
+
+/**
+ * Created by Isabell on 16.04.2018.
+ */
+public class SimpleNavigationPilot implements NavigationInterface{
+
+    private DifferentialPilot pilot;
+    private Direction facing;
+
+    public SimpleNavigationPilot(DifferentialPilot pilot){
+        this.facing = Direction.NORTH;
+        this.pilot = pilot;
+    }
+
+    @Override
+    public void rotateDegrees(double degrees) {
+        pilot.rotate(degrees);
+    }
+
+    @Override
+    public void rotateDirection(Direction direction) {
+        double angle = this.facing.degrees(direction);
+        pilot.rotate(angle);
+    }
+
+    @Override
+    public int driveForward() {
+        pilot.travel(20);
+        return 0;
+    }
+
+    @Override
+    public int driveDirection(Direction direction) {
+        this.rotateDirection(direction);
+        return this.driveForward();
+    }
+}
